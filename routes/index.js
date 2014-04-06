ba71ef3ec7e9877e0194de42345d8ebb61168fc2
Ansal <ansal@bssatech.com>
2014-04-07 00:02:05 +0530

    completed html to json for codeacademy

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,74 @@
-
-/*
- * GET home page.
- */
+var request = require('request');
+var cheerio = require('cheerio');
 
 exports.index = function(req, res){
-  res.send('Hello');
+
+  var username = req.params.username;
+  var requestObject = {
+    uri: 'http://www.codeacademy.com/' + username,
+    method: 'GET',
+    timeout: 10000,
+    followRedirect: true,
+    maxRedirects: 10  
+  };
+
+  function fetchUserInfo(error, response, html) {
+    function fetchAchievements (error, response, html) {
+
+      var $ = cheerio.load(html);
+        
+      // get achievement badges
+      var userAchievements = $('.achievement');
+      var achievements = [];
+      for(var i = 0; i < userAchievements.length; i++) {
+        achievements.push({
+          name: $(userAchievements[i]).find('.name').text(),
+          image: $(userAchievements[i]).find('.badge').css('background-image').slice(4, -1)
+        });
+      }
+      json.achievements = achievements;
+
+      res.json(json);
+    }
+    if(error) {
+      res.send({
+        error: true,
+        message: 'Sorry guys, I cannot convert for JSON for that username'
+      });
+      return;
+    }
+    var $ = cheerio.load(html);
+    
+    // get the full name and location
+    var json = {
+      name: $('.full-name').text(),
+      location: $('.location-content').text()
+    };
+
+    // get point
+    var points = $('.stat-count');
+    json.points = $(points[0]).text();
+
+    // get tracks
+    var userTracks = $('.track-progress');
+    var tracks = [];
+    for(var i = 0; i < userTracks.length; i++) {
+      tracks.push({
+       title: $(userTracks[i]).find('.track-name').text(),
+      });
+    }
+    json.tracks = tracks;
+
+    var requestObject = {
+      uri: 'http://www.codecademy.com/users/' + username + '/achievements',
+      method: 'GET',
+      timeout: 10000,
+      followRedirect: true,
+      maxRedirects: 10  
+    };
+
+    request(requestObject, fetchAchievements.bind({json: this.json}));
+  }
+
+  request(requestObject, fetchUserInfo);
 };
